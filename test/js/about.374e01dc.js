0da8e7d84e449ba417f896a5fb8ada5a599c4dcc
Mr.li <34509280+lipuqing@users.noreply.github.com>
2023-02-14 15:05:06 +0800

    创建

---
+++ test/js/about.374e01dc.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23");const c={class:"about"},o=Object(e["e"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(e["r"])(),Object(e["d"])("div",c,s)}var b=a("6b0d"),r=a.n(b);const i={},d=r()(i,[["render",u]]);t["default"]=d}}]);
