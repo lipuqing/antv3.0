7d8e3b05f38236e637df6410fde8082f7a407d1b
sean1994 <17638198892@163.com>
2023-02-15 13:28:18 +0800

    面包屑

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,7 @@
         <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item>Home</a-breadcrumb-item>
           <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
+          <a-breadcrumb-item>{{ array[selectedKeys1].name }}</a-breadcrumb-item>
         </a-breadcrumb>
         <a-layout-content
           :style="{
